fe72d39d14694bc754333fcbe6cb24c03d68f1c8
kvnfstn <kevin.faustini@epfl.ch>
2023-10-20 13:13:51 +0000

    add/update resp/part data in firestore + storage methods

--- collection/functions/google_firebase_helper.private.js
+++ collection/functions/google_firebase_helper.private.js
@@ -1,21 +1,20 @@
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore, FieldPath, FieldValue, Filter } = require("firebase-admin/firestore");
-const { getStorage } = require("firebase-admin/storage")
-
+const { getStorage } = require("firebase-admin/storage");
+//+ check how to use dotenv
 // Your web app's Firebase configuration //! this is temporary, will then be edited out
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBxY9M0NJT0o1UOWMaGWDkG-tV3WjLLytg",
   authDomain: "waxal-kera.firebaseapp.com",
   projectId: "waxal-kera",
   storageBucket: "waxal-kera.appspot.com",
   messagingSenderId: "58578486226",
-  appId: "1:58578486226:web:cc5b5524ff63433631fe0b"
+  appId: "1:58578486226:web:cc5b5524ff63433631fe0b",
 };
 
 const app = initializeApp(FIREBASE_CONFIG);
 const db = getFirestore(app);
-const storage = getStorage(app)
-
+const storage = getStorage(app);
 
 exports.getParticipantsCollectionRef = async () => {
   // console.log(`Fetching participants collection`);
@@ -64,4 +63,53 @@ exports.getResponseDocRef = async (responseId) => {
   } catch (error) {
     throw error;
   }
+};
+
+exports.getStorageBucket = async () => {
+  try {
+    return storage.bucket();
+  } catch (e) {
+    throw e;
+  }
+};
+
+exports.updateParticipantAfterResponse = async (participantRef, participantData) => {
+  console.log("Applying change to participant data");
+  participantRef
+    .update(participantData)
+    .then(() => {
+      console.log("Successfully updated participant data in firestore");
+    })
+    .catch((err) => {
+      console.error("Error while updating participant data", err);
+    });
+};
+
+/**
+ * Update Response and Participant tables after uploading media.
+ * @param {string} participantRef Reference to the firestore participant item.
+ * @param {object} participantData Full participant object.
+ * @param {string} promptId ID of the prompt being responded to.
+ * @param {string} bucket GCP storage bucket name.
+ * @param {number} duration Duration in seconds of the audio response.
+ */
+exports.addParticipantResponse = async (participantRef, promptId, dlLink, duration) => {
+  const varsHelper = require(Runtime.getFunctions()["vars_helper"].path);
+  console.log("Adding response to sheet");
+  const responsesCol = await getResponsesCollectionRef();
+  const promptCol = await getPromptsCollectionRef();
+  const language = varsHelper.getVar("speech-language");
+
+  responsesCol.add({
+    storage_link: dlLink,
+    duration: duration,
+    language: language,
+    participant_path: participantRef.path,
+    prompt_path: promptCol.doc(promptId).path,
+    response_date: new Date().toISOString(),
+    transcription_counts: {
+      [language]: 1,
+    },
+    status: "New",
+  }).then()
 };
